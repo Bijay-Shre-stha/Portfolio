0d6cfe7f482eff3ff2462f2be1b353bef374353e
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-09-19 19:15:41 +0545

    navbar final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <a href="/" className="navbar-brand"><img className='navbar__logo' src="https://doko.dwit.edu.np/assets/images/doko_logo.png" alt="" /></a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light position-sticky">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand"><img className='navbar__logo' src="https://doko.dwit.edu.np/assets/images/doko_logo.png" alt="myLogo" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
