9a7ecf525e2b611b7126ab6b7b3e906522acf164
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-09-22 20:26:37 +0545

    added logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import './Navbar.css';
+import zero from '../Images/Zero.png'
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light position-sticky">
             <div className="container-fluid">
-                <a href="/" className="navbar-brand"><img className='navbar__logo' src="https://doko.dwit.edu.np/assets/images/doko_logo.png" alt="myLogo" /></a>
+                <a href="/" className="navbar-brand"><img className='navbar__logo' src={zero} alt="myLogo" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
